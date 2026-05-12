0b212ad3f1c0e5513501a5ce2e7a084ad67e3eda
nanayyayaa <jiejueejuju@gmail.com>
2026-05-12 11:07:29 +0700

    commit 12

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -65,3 +65,13 @@ public:
                 current = current->next;
             }
             cout << endl;
+
+        } // return the value of the top node
+    }
+
+    // isEmpty operation: check of the stack is empty
+    bool isEmpty()
+    {
+        return top == NULL; //Return true if the top pointer is NULL, indicating an empty stack
+    }
+};
