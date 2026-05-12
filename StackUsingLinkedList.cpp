8827c615e5e6209191e6e6ba5ae43d6d7b482624
nanayyayaa <jiejueejuju@gmail.com>
2026-05-12 11:06:30 +0700

    commit 11

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -57,3 +57,11 @@ public:
             cout << "List is Empty." << endl;
         }
         else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
